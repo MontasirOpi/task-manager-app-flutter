7bf1d1866b99debb2293b8fb89fe3e6b82c0fbcb
MontasirOpi <montasiropi@gmail.com>
2024-11-14 15:57:22 +0600

    Add new Task api and auth

--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -2,7 +2,10 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:task_manager/app.dart';
 import 'package:task_manager/data/models/network_response.dart';
+import 'package:task_manager/ui/controller/auth_controller.dart';
+import 'package:task_manager/ui/screens/sign_in_screen.dart';
 
 class NetworkCaller {
   static Future<NetworkResponse> getRequest({required String url}) async {
@@ -18,7 +21,16 @@ class NetworkCaller {
           statusCode: response.statusCode,
           responseData: decodeData,
         );
-      } else {
+      }
+      else if (response.statusCode == 401) {
+        _moveToLogin();
+         return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+          errorMessage: 'Unauthenticated!'
+         );
+      }
+       else {
         return NetworkResponse(
           isSuccess: false,
           statusCode: response.statusCode,
@@ -37,25 +49,38 @@ class NetworkCaller {
       {required String url, Map<String, dynamic>? body}) async {
     try {
       Uri uri = Uri.parse(url);
+      Map<String, String> headers = {
+        'Content-Type': 'application/json',
+        'token': AuthController.accessToken.toString(),
+      };
       debugPrint(url);
-      final Response response = await post(uri,
-          headers: {'Content-Type': 'application/json'},
-          body: jsonEncode(body));
+      printRequest(url, body, headers);
+      final Response response =
+          await post(uri, headers: headers, body: jsonEncode(body));
+
       printResponse(url, response);
       if (response.statusCode == 200) {
         final decodeData = jsonDecode(response.body);
-        if(decodeData['status']=='fail'){
+        if (decodeData['status'] == 'fail') {
           return NetworkResponse(
-          isSuccess: false,
-          statusCode: response.statusCode,
-          errorMessage: decodeData['data'],
-        );
+            isSuccess: false,
+            statusCode: response.statusCode,
+            errorMessage: decodeData['data'],
+          );
         }
         return NetworkResponse(
           isSuccess: true,
           statusCode: response.statusCode,
           responseData: decodeData,
         );
+      } 
+      else if (response.statusCode == 401) {
+        _moveToLogin();
+         return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+          errorMessage: 'Unauthenticated!'
+         );
       } else {
         return NetworkResponse(
           isSuccess: false,
@@ -71,9 +96,27 @@ class NetworkCaller {
     }
   }
 
+  static void printRequest(
+    String url,
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? headers,
+  ) {
+    debugPrint(
+      'REQUEST:\nURL: $url\nBODY: $body\nHEADERS: $headers',
+    );
+  }
+
   static void printResponse(String url, Response response) {
     debugPrint(
       'URL: $url\nRESPONS CODE:${response.statusCode}\nBODY: ${response.body}',
     );
+  }
+
+  static Future<void> _moveToLogin() async {
+    await AuthController.clearUserData();
+    Navigator.pushAndRemoveUntil(
+        TaskManagerApp.navigatorKey.currentContext!,
+        MaterialPageRoute(builder: (context) => const SignInScreen()),
+        (p) => false);
   }
 }
--- lib/ui/screens/add_new_task_screen.dart
+++ lib/ui/screens/add_new_task_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/data/models/network_response.dart';
+import 'package:task_manager/data/services/network_caller.dart';
+import 'package:task_manager/data/utils/urls.dart';
+import 'package:task_manager/ui/widgets/centered_circular_progress_indicator.dart';
+import 'package:task_manager/ui/widgets/snack_bar_message.dart';
 import 'package:task_manager/ui/widgets/tm_app_bar.dart';
 
 class AddNewTaskScreen extends StatefulWidget {
@@ -9,6 +14,12 @@ class AddNewTaskScreen extends StatefulWidget {
 }
 
 class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
+  final TextEditingController _titleTEController = TextEditingController();
+  final TextEditingController _descriptionTEController =
+      TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _addNewTaskInProgress = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,38 +27,90 @@ class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 42),
-              Text(
-                'Add New Task',
-                style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                      fontWeight: FontWeight.w600,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 42),
+                Text(
+                  'Add New Task',
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                ),
+                const SizedBox(height: 24),
+                TextFormField(
+                  controller: _titleTEController,
+                  decoration: const InputDecoration(
+                    hintText: 'Title',
+                  ),
+                  validator: (String? value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return 'Enter a value';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 8),
+                Visibility(
+                  visible: !_addNewTaskInProgress,
+                  replacement: const CenteredCircularProgressIndicator(),
+                  child: TextFormField(
+                    controller: _descriptionTEController,
+                    maxLines: 5,
+                    decoration: const InputDecoration(
+                      hintText: 'Description',
                     ),
-              ),
-              const SizedBox(height: 24),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Title',
+                    validator: (String? value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter a value';
+                      }
+                      return null;
+                    },
+                  ),
                 ),
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                maxLines: 5,
-                decoration: const InputDecoration(
-                  hintText: 'Description',
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: _onTapSubmitButton,
+                  child: const Icon(Icons.arrow_circle_right_outlined),
                 ),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Icon(Icons.arrow_circle_right_outlined),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _onTapSubmitButton() {
+    if (_formKey.currentState!.validate()) {
+      _addNewTask();
+    }
+  }
+
+  Future<void> _addNewTask() async {
+    _addNewTaskInProgress = true;
+    setState(() {});
+    Map<String, dynamic> requestBody = {
+      'title': _titleTEController.text.trim(),
+      'description': _descriptionTEController.text.trim(),
+      'status': 'New',
+    };
+    final NetworkResponse response =
+        await NetworkCaller.postRequest(url: Urls.addNewTask,body: requestBody);
+        _addNewTaskInProgress=false;
+        setState(() {});
+        if(response.isSuccess){
+          _clearTextFilds();
+          showSnackBarMessage(context, 'New Task added');
+        }else{
+           showSnackBarMessage(context, response.errorMessage,true);
+        }
+  }
+  void _clearTextFilds(){ 
+    _titleTEController.clear();
+    _descriptionTEController.clear();
+
   }
 }
